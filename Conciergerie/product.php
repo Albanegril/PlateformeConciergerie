39fae7f613fd6aa3dc5a8071a1e53f4db06a55bd
AfuaDOM <sandradomyes@gmail.com>
2018-12-04 18:30:15 +0100

    product page

--- Conciergerie/product.php
+++ Conciergerie/product.php
@@ -18,8 +18,78 @@
 
           <div class="container" id="Product">
                   <div class="row">
-                    
-                  </div>     
+                    <div class="col s6">
+                      <div class="card small">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="images/scandal.jpeg"><!-- Image en 500px-->
+                        </div>
+                        <div class="card-reveal">
+                          <span class="card-title grey-text text-darken-4">Description<i class="material-icons right">close</i></span>
+                          <p>Le produit ...</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col s4">
+                      <span>
+                        <h5>Nom produit</h5>
+                        <h4 class="grey-text">code258-25</h4>
+                        <h3 class="red-text">30,00€</h3>
+                        <ul class="collapsible brown lighten-4">
+                          <li class="active">
+                              <div class="collapsible-header">Type de produit</div>
+                              <div class="collapsible-body"><span>juii</span></div>
+                          </li>
+                          <li>
+                              <div class="collapsible-header">Produit inclus</div>
+                              <div class="collapsible-body"><span>juii</span></div>
+                          </li>
+                        </ul>
+                      
+                      </span>
+
+                      <div class="input-field">
+                        <select>
+                          <option value="1" disabled selected>Option 1</option>
+                          <option value="2">Option 2</option>
+                          <option value="3">Option 3</option>
+                        </select>
+                      </div>
+                    </div> 
+                    <div class="col s1">
+                      <br>
+                        <a href="#">
+                        <!--<img src="">-->
+                        <h9>logo marque</h9>
+                      </a>            
+                    </div>  
+                    <div class="col s1">
+                      <br>
+                        <a href="#">
+                        <!--<img src="">-->
+                        <h9>logo fournisseur</h9>
+                      </a>            
+                    </div>   
+                
+                  <table>
+                    <thead>
+                        <tr>
+                          <th>Brut</th>
+                          <th>Livaison</th>
+                          <th>NetTask</th>
+                      </tr>
+                  </thead>
+
+                  <tbody>
+                    <tr>
+                      <td>52</td>
+                      <td>25</td>
+                      <td>36</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+            </div>
+          </div>     
           </div>
 
       <?php include_once ("footer.php"); ?>
