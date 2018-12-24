30f88f1bac070d8c0db39a34d9279ee73a7a711b
Unknown <albane.gril@free.fr>
2018-12-24 14:56:37 +0100

    fix panier

--- Conciergerie/panier.php
+++ Conciergerie/panier.php
@@ -1,5 +1,44 @@
-<div class="card small grey lighten-5 col l2 s10 offset-s1"  id="Panier">
-    <div class="card-stacked">
-      <p>PANIER</p>
-    </div>
+<div class="col l2 s10 offset-s1"  id="Panier">
+	<div class="row">
+		<ul class="collapsible grey lighten-5">
+		    <li>
+		      <div class="collapsible-header col s12">
+				<!--
+				<div class="row">
+			        <div class="col s12">
+			        -->
+			          <ul class="tabs">
+			            <li class="tab col s6"><a class="active" href="#Commande">Panier</a></li>
+			            <li class="tab col s6"><a href="#Client">Client</a></li>
+			          </ul>
+			          <!--
+			        </div>
+			    </div>
+			    -->
+		      </div>
+			    <div class="collapsible-body">
+					    <div class="panel" id="Commande">
+					    	<ul>
+					      		<li><h6>Nom Produit</h6><span>PRIX</span></li>
+					      		<li><h6>Nom Produit</h6><span>PRIX</span></li>
+					      	</ul>
+					      	<div class="row">
+							  	<div class="col s12 right-align">
+							    	<input type="submit" value="Commander" name="commander" id="commander" class="waves-effect waves-light btn yellow darken-2" />
+							  	</div>
+							</div>
+					    </div>
+
+					    <div class="panel" id="Client">
+					      	<p>Nom</p>
+					      	<p>Numéro</p>
+					      	
+						  	<div class="col s12 right-align">
+						    	<input type="submit" value="Commander" name="commander" id="commander" class="waves-effect waves-light btn yellow darken-2" />
+						  	</div>		      	
+					    </div>
+			    </div>
+		    </li>
+		</ul>    
+	</div> 	
 </div>
